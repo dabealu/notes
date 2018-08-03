23c450a96278657a23113c108e0584219e07bb1d
dabealu <dabealu@gmail.com>
2018-08-03 23:54:02 +0300

    i3: add translate script

---
+++ i3/translate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+SEL=$(xsel -o)
+TR=$( curl -sH "User-agent: Mozilla/5.0" \
+      "http://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=ru&dt=t&q=$SEL" \
+      | awk -F ',' '{gsub("\"|\\[", "", $1); print $1}' )
+
+notify-send "$SEL" "$TR"
+
