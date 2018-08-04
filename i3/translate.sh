dfc16ecd1ed334eab7624b833eeb0db4efe0342c
dabealu <dabealu@gmail.com>
2018-08-04 13:33:46 +0300

    i3: fix translate script

--- i3/translate.sh
+++ i3/translate.sh
@@ -2,7 +2,7 @@
 
 SEL=$(xsel -o)
 TR=$( curl -sH "User-agent: Mozilla/5.0" \
-      "http://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=ru&dt=t&q=$SEL" \
+      "http://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=ru&dt=t&q=$(echo $SEL | tr ' ' '+')" \
       | awk -F ',' '{gsub("\"|\\[", "", $1); print $1}' )
 
 notify-send "$SEL" "$TR"
