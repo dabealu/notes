81d84c41f30b9af7ec5016141420394ac584b7a4
dabealu <dabealu@gmail.com>
2019-01-01 20:04:44 +0800

    i3: resources icons

--- i3/resources.sh
+++ i3/resources.sh
@@ -5,6 +5,7 @@
 ROOTFS=$(df --output='pcent' / | awk 'NR==2 {gsub("%",""); print}')
 LA1_CORE=$(awk '/cpu cores/ {c=$4} $1 ~ /^[0-9]+\.[0-9]+/ {l=$1} END {printf("%.1f", l/c)}' /proc/cpuinfo /proc/loadavg)
 MEM_USED=$(awk '$1 ~ /MemTotal/ {t=$2} $1 ~ /MemAvailable/ {a=$2} END {printf("%.0f", (1-a/t)*100 )}' /proc/meminfo)
-PING_STATE=$(ping -w 1 -qc 1 8.8.8.8 >/dev/null && echo  || echo )
+PING_STATE=$(ping -w 1 -qc 1 8.8.8.8 >/dev/null && echo  || echo )
 
 echo "$PING_STATE      $LA1_CORE      $MEM_USED      $ROOTFS"
+
