23d8cc6faed5c576ef84c4d68a16671dc2cf2efb
Dmitry Andronov <dabealu@gmail.com>
2021-03-10 12:29:41 +0800

    upd i3 config

--- i3/resources.sh
+++ i3/resources.sh
@@ -7,4 +7,4 @@ LA1_CORE=$(awk '/cpu cores/ {c=$4} $1 ~ /^[0-9]+\.[0-9]+/ {l=$1} END {printf("%.
 MEM_USED=$(awk '$1 ~ /MemTotal/ {t=$2} $1 ~ /MemAvailable/ {a=$2} END {printf("%.0f", (1-a/t)*100 )}' /proc/meminfo)
 PING=$(ping -w 1 -qc 1 1.1.1.1 >/dev/null && echo ok || echo fail)
 
-echo "ping: $PING     la1: $LA1_CORE     mem: $MEM_USED     rootfs: $ROOTFS"
+echo "ping $PING     la1 $LA1_CORE     mem $MEM_USED     rootfs $ROOTFS"
